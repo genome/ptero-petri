798a4200418954975038d9ff0d12b7f3707e97b8
Mark Burnett <mark.m.burnett@gmail.com>
2014-03-13 15:51:31 -0500

    add port and debug options to devserver

--- ptero_petri/api/wsgi.py
+++ ptero_petri/api/wsgi.py
@@ -1,7 +1,16 @@
 from ptero_petri.api import application
+import argparse
 
 app = application.create_app()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--debug', action='store_true', default=False)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    app.run()
+    args = parse_args()
+    app.run(port=args.port, debug=args.debug)
