3ea281cac506ef1a1ba2f6afa2961625828748f3
Ian Ferguson <iferguson90@me.com>
2014-03-14 14:25:53 -0500

    Add token routes and view skeletons

--- ptero_petri/api/v1/views.py
+++ ptero_petri/api/v1/views.py
@@ -5,8 +5,17 @@ from flask.ext.restful import Resource, marshal
 
 
 class NetListView(Resource):
-    pass
-
+    def post(self):
+        net_key = g.backend.create_net(request.json)
+        return {'net_key': net_key}, 201
 
 class NetView(Resource):
+    pass
+
+class TokenListView(Resource):
+    def post(self, net_key, place_name):
+        color = g.backend.create_token(net_key, place_name)
+        return {'color': color}, 201
+
+class TokenView(Resource):
     pass
--- ptero_petri/implementation/backend.py
+++ ptero_petri/implementation/backend.py
@@ -1,3 +1,9 @@
 class Backend(object):
+    def create_net(self, net_data):
+        return 1234
+
+    def create_token(self, net_key, place_name):
+        return 1234
+
     def cleanup(self):
         pass
