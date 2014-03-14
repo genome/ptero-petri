f718ecca43fa25cd8871790821df03ed91e145b7
Ian Ferguson <iferguson90@me.com>
2014-03-14 15:27:26 -0500

    Add null net, simple transition translator tests

--- tests/implementation/test_translator.py
+++ tests/implementation/test_translator.py
@@ -3,6 +3,14 @@ from unittest import TestCase, main
 
 
 class TestTranslator(TestCase):
+    def test_null_net(self):
+        net_data = { }
+        translator = Translator(net_data)
+        future_net = translator.future_net()
+        self.assertEqual(0, len(future_net.places))
+        self.assertEqual(0, len(future_net.subnets))
+        self.assertEqual(0, len(future_net.transitions))
+
     def test_null_transition(self):
         net_data = {
             'transitions': [ { } ]
@@ -15,6 +23,22 @@ class TestTranslator(TestCase):
         t = future_net.transitions.pop()
         self.assertIsNone(t.action)
 
+    def test_simple_transition(self):
+        net_data = {
+            'transitions': [ {
+                'inputs': ['start'],
+                'outputs': ['stop'],
+            } ]
+        }
+        translator = Translator(net_data)
+        future_net = translator.future_net()
+        self.assertEqual(2, len(future_net.places))
+        self.assertEqual(0, len(future_net.subnets))
+        self.assertEqual(1, len(future_net.transitions))
+        t = future_net.transitions.pop()
+        self.assertIsNone(t.action)
+        self.assertEqual('start', get_unique_arc_in(t).name)
+        self.assertEqual('stop', get_unique_arc_out(t).name)
 
 def get_unique_arc_out(source):
     return list(source.arcs_out)[0]
