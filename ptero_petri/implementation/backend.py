93907777195c9efbb088764b8e7923a38d6e110b
Ian Ferguson <iferguson90@me.com>
2014-03-24 15:32:05 -0500

    Add a bit of substance to create_net

--- ptero_petri/implementation/backend.py
+++ ptero_petri/implementation/backend.py
@@ -1,6 +1,15 @@
+from .translator import Translator
+from .builder import Builder
+import redis
+
 class Backend(object):
     def create_net(self, net_data):
-        return 1234
+        translator = Translator(net_data)
+        future_net = translator.future_net()
+        conn       = redis.Redis()
+        builder    = Builder(conn)
+        stored_net = builder.store(future_net, {}, {})
+        return stored_net.key
 
     def create_token(self, net_key, place_name):
         return 1234
