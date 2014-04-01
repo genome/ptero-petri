a419b7b031f2e2ac7655f6ff01b9f9e524274fe0
Mark Burnett <mark.m.burnett@gmail.com>
2014-04-01 08:02:51 -0500

    re-use single redis connection in factory

--- ptero_petri/implementation/factory.py
+++ ptero_petri/implementation/factory.py
@@ -7,15 +7,18 @@ __all__ = ['Factory']
 class Factory(object):
     def __init__(self):
         self._initialized = False
+        self._redis = None
 
     def create_backend(self):
         self._initialize()
-        return backend.Backend(redis_connection=redis.Redis())
+        return backend.Backend(redis_connection=self._redis)
 
     def purge(self):
         self._initialize()
+        self._redis.flushall()
 
     def _initialize(self):
         # Lazy initialize to be pre-fork friendly.
         if not self._initialized:
             self._initialized = True
+            self._redis = redis.Redis()
