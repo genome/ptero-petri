7eb8926cea9335dcff5aa4347af405e96a526788
Ian Ferguson <iferguso@genome.wustl.edu>
2014-06-10 16:30:46 -0500

    Pull in changes from fork's tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,6 @@ import errno
 import os
 import psutil
 import signal
-import subprocess
 import sys
 import time
 
@@ -11,8 +10,7 @@ NUM_WORKERS = 4
 
 
 instance = None
-grandchildren = []
-descendents = []
+descendents = {}
 
 
 def mkdir_p(path):
@@ -31,11 +29,11 @@ def wait_time():
     else:
         return 3
 
+def this_dir():
+    return os.path.dirname(__file__)
 
 def procfile_path():
-    return os.path.join(os.path.dirname(__file__),
-            'scripts', 'Procfile')
-
+    return os.path.join(this_dir(), 'scripts', 'Procfile')
 
 def service_command_line():
     return ['honcho', '-f', procfile_path(), 'start']
@@ -43,8 +41,6 @@ def service_command_line():
 
 def setUp():
     global instance
-    global grandchildren
-    global descendents
 
     logdir = 'var/log'
     mkdir_p(logdir)
@@ -52,27 +48,13 @@ def setUp():
     errlog = open(os.path.join(logdir, 'honcho.err'), 'w')
 
     if not os.environ.get('SKIP_PROCFILE'):
-        instance = subprocess.Popen(service_command_line(),
+        instance = psutil.Popen(service_command_line(),
                 shell=False, stdout=outlog, stderr=errlog)
         time.sleep(wait_time())
         if instance.poll() is not None:
             raise RuntimeError("honcho instance terminated prematurely")
-        grandchildren = get_grandchildren()
-        descendents = get_descendents()
-
-
-
-def travis_ci_cleanup():
-    if not signal_processes(grandchildren, signal.SIGINT):
-        return
-
-    time.sleep(1)
-    if not signal_processes(descendents, signal.SIGINT):
-        return
-
-    time.sleep(3)
-    signal_processes(descendents, signal.SIGKILL)
-
+        else:
+            descendents = get_descendents()
 
 def signal_processes(processes, sig):
     signaled_someone = False
@@ -85,30 +67,29 @@ def signal_processes(processes, sig):
 
     return signaled_someone
 
-
-def get_grandchildren():
-    children = psutil.Process(instance.pid).get_children(recursive=False)
-    grandchildren = []
-    for child in children:
-        grandchildren.extend(child.get_children(recursive=False))
-    return grandchildren
-
-
 def get_descendents():
-    return psutil.Process(instance.pid).get_children(recursive=True)
+    processes = psutil.Process(instance.pid).get_children(recursive=True)
+    return dict([(x.pid, x) for x in processes])
+
+def cleanup():
+    ec = instance.poll()
+    if ec is None:
+        descendents.update(get_descendents())
+
+        instance.wait(timeout=2)
+
+        if not signal_processes(descendents, signal.SIGINT):
+            return
+
+        time.sleep(3)
+        signal_processes(descendents, signal.SIGKILL)
+
+        instance.send_signal(signal.SIGINT)
+    else:
+        sys.stderr.write('Unexpected exit of services:  code = (%s)\n' % ec)
 
 
 # NOTE If this doesn't run then honcho will be orphaned...
 def tearDown():
     if not os.environ.get('SKIP_PROCFILE'):
-        if os.environ.get('TRAVIS'):
-            try:
-                travis_ci_cleanup()
-            except psutil.NoSuchProcess:
-                pass
-
-        ec = instance.poll()
-        if ec is None:
-            instance.send_signal(signal.SIGINT)
-        else:
-            sys.stderr.write('Unexpected exit of services:  code = (%s)\n' % ec)
+        cleanup()
