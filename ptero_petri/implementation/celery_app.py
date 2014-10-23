a1081b7bbddea976c73f282a96b76666d74c84c5
Mark Burnett <mark.m.burnett@gmail.com>
2014-10-23 08:01:42 -0500

    add celery app

---
+++ ptero_petri/implementation/celery_app.py
@@ -0,0 +1,24 @@
+import celery
+import os
+
+
+TASK_PATH = 'ptero_petri.implementation.celery_tasks'
+
+
+app = celery.Celery('PTero-petri-celery', include=TASK_PATH)
+
+
+_DEFAULT_CELERY_CONFIG = {
+    'CELERY_BROKER_URL': 'amqp://localhost',
+    'CELERY_RESULT_BACKEND': 'redis://localhost',
+    'CELERY_ACCEPT_CONTENT': ['json'],
+    'CELERY_ACKS_LATE': True,
+    'CELERY_RESULT_SERIALIZER': 'json',
+    'CELERY_TASK_SERIALIZER': 'json',
+    'CELERYD_PREFETCH_MULTIPLIER': 10,
+}
+for var, default in _DEFAULT_CELERY_CONFIG.iteritems():
+    if var in os.environ:
+        app.conf[var] = os.environ[var]
+    else:
+        app.conf[var] = default
