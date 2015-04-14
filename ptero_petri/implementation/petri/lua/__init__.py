6563d5b9fb5b88cb0f75199e433e198cd4617de3
David Morton <davidlmorton@gmail.com>
2015-04-14 23:00:43 +0000

    Implement templating support for lua scripts
    {{foo}} will be replaced with the contents of the file foo.template.
    
    You can also do {{environment['FOO']}} to access environment variables
    inside the .lua files and inside the .template files

--- ptero_petri/implementation/petri/lua/__init__.py
+++ ptero_petri/implementation/petri/lua/__init__.py
@@ -1,8 +1,28 @@
-import os.path
+import os
+from os import environ
+import jinja2
 
 BASE_PATH = os.path.dirname(__file__)
 
 
-def load(module_name):
+def get_template_data():
+    result = {}
+    for filename in os.listdir(BASE_PATH):
+        module, extension = os.path.splitext(filename)
+        if extension.endswith('template'):
+            fullpath = os.path.join(BASE_PATH, filename)
+            template = jinja2.Template(open(fullpath).read())
+            result[module] = template.render(environment=environ)
+    result['environment'] = environ
+    return result
+
+
+TEMPLATE_DATA = get_template_data()
+
+
+def load(module_name, template_data=TEMPLATE_DATA):
     filename = os.path.join(BASE_PATH, '%s.lua' % module_name)
-    return open(filename).read()
+
+    with open(filename) as f:
+        template = jinja2.Template(f.read())
+    return template.render(**template_data)
--- tests/implementation/petri/test_lua.py
+++ tests/implementation/petri/test_lua.py
@@ -15,6 +15,23 @@ class TestLoadLua(unittest.TestCase):
             script = lua.load('alpha')
         self.assertEqual(script, 'AAA')
 
+    def test_file_with_template(self):
+        with mock.patch('ptero_petri.implementation.petri.lua.BASE_PATH',
+                        self.base_path):
+            script = lua.load('example_with_template',
+                    template_data={'example': 'B'})
+        self.assertEqual(script, 'A\nB\nC')
+
+    def test_get_template_data(self):
+        os.environ['EXAMPLE_ENVIRONMENT_VARIABLE'] = 'ABC'
+        with mock.patch('ptero_petri.implementation.petri.lua.BASE_PATH',
+                        self.base_path):
+            with mock.patch('ptero_petri.implementation.petri.lua.environ',
+                    os.environ):
+                data = lua.get_template_data()
+        self.assertEqual(data.get('environment'), os.environ)
+        self.assertEqual(data.get('example'), 'ABC')
+
 
 if __name__ == "__main__":
     unittest.main()
