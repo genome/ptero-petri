672dfea90e7a7794a91d595a34c83493c2a7cdc6
Michael J. Kiwala <michael.kiwala@gmail.com>
2015-08-13 12:32:24 -0500

    Use ptero_common to get celery configuration

--- ptero_petri/implementation/celery_app.py
+++ ptero_petri/implementation/celery_app.py
@@ -2,8 +2,8 @@ from . import celery_tasks  # nopep8
 from . import storage
 from celery.signals import worker_init, setup_logging
 from ptero_common.logging_configuration import configure_celery_logging
+from ptero_common.celery.utils import get_config_from_env
 import celery
-import os
 
 
 TASK_PATH = 'ptero_petri.implementation.celery_tasks'
@@ -20,20 +20,8 @@ app.conf['CELERY_ROUTES'] = (
     },
 )
 
-_DEFAULT_CELERY_CONFIG = {
-    'CELERY_BROKER_URL': 'amqp://localhost',
-    'CELERY_RESULT_BACKEND': 'redis://localhost',
-    'CELERY_ACCEPT_CONTENT': ['json'],
-    'CELERY_ACKS_LATE': True,
-    'CELERY_RESULT_SERIALIZER': 'json',
-    'CELERY_TASK_SERIALIZER': 'json',
-    'CELERYD_PREFETCH_MULTIPLIER': 10,
-}
-for var, default in _DEFAULT_CELERY_CONFIG.iteritems():
-    if var in os.environ:
-        app.conf[var] = os.environ[var]
-    else:
-        app.conf[var] = default
+config = get_config_from_env('PETRI')
+app.conf.update(config)
 
 
 @setup_logging.connect
