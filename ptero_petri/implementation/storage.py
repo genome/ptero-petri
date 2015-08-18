2d5f36a63f5d4825b298463c7be75d837dfe95ba
David Morton <davidlmorton@gmail.com>
2015-08-18 15:44:05 +0000

    Make environment variable names consistent
    PTERO_<SERVICE>_REDIS_PORT is used in all services by tests to spin up a
    redis instance.  To disambiguate the REDIS needed for CELERY and the one
    needed to store informations about NETs, I've created versions of those
    env vars that specify NET_REDIS instead of just REDIS.

--- ptero_petri/implementation/storage.py
+++ ptero_petri/implementation/storage.py
@@ -5,12 +5,12 @@ import redis
 __all__ = ['get_connection']
 
 
-_HOST = os.environ.get('PTERO_PETRI_REDIS_HOST')
-_PASSWORD = os.environ.get('PTERO_PETRI_REDIS_PASSWORD')
-_PORT = int(os.environ.get('PTERO_PETRI_REDIS_PORT', 6379))
-_SOCKET_PATH = os.environ.get('PTERO_PETRI_REDIS_PATH')
+_HOST = os.environ.get('PTERO_PETRI_NET_REDIS_HOST')
+_PASSWORD = os.environ.get('PTERO_PETRI_NET_REDIS_PASSWORD')
+_PORT = int(os.environ.get('PTERO_PETRI_NET_REDIS_PORT', 6379))
+_SOCKET_PATH = os.environ.get('PTERO_PETRI_NET_REDIS_PATH')
 
-DEFAULT_TTL = int(os.environ['PTERO_PETRI_REDIS_DEFAULT_TTL'])
+DEFAULT_TTL = int(os.environ['PTERO_PETRI_NET_REDIS_DEFAULT_TTL'])
 COMMANDS_THAT_CAN_ADD_KEYS = set(['lpush', 'rpush', 'set', 'setnx', 'incr',
     'decr', 'incrby', 'decrby', 'hincrby', 'hset', 'hsetnx', 'hmset', 'sadd'])
 
