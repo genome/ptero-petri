6c29e70c7468dd0ffda30541b3381fa38b984351
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-12-17 22:46:23 +0000

    flake8 / import problems

--- ptero_petri/api/application.py
+++ ptero_petri/api/application.py
@@ -1,6 +1,10 @@
 from . import v1
 from ..implementation.factory import Factory
+from ptero_common import nicer_logging
 import flask
+
+
+LOG = nicer_logging.getLogger(__name__)
 
 
 __all__ = ['create_app']
@@ -34,7 +38,9 @@ def _attach_factory_to_app(factory, app):
             flask.g.backend = factory.create_backend()
         except:
             LOG.exception("Exception occured while creating backend")
-            return jsonify({"error": "Internal Server Error: could not create backend"}), 500
+            return flask.jsonify({
+                "error": "Internal Server Error: could not create backend"
+                }), 500
 
     @app.teardown_request
     def teardown_request(exception):
